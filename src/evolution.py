b70f8b56fc6201352c37047a509dfb14326ed8bd
Christopher Rieß <chris.mriess@gmail.com>
2024-03-08 19:03:42 +0100

    cleanup

--- src/evolution.py
+++ src/evolution.py
@@ -302,9 +302,9 @@ def select (selection_size):
         fitnesses = process_pool.starmap(evaluate_individual, enumerate(_population), batches)
             
         process_pool.close()
-        
-    individual_fitnesses.insert(1, "fitness", fitnesses)
-    individual_fitnesses = individual_fitnesses.sort_values("fitness", ascending =  False).head(selection_size)
+                
+    individual_fitnesses.insert(1, "fitness", fitnesses)    
+    individual_fitnesses = individual_fitnesses.sort_values("fitness", ascending = False).head(selection_size)
     
     _population = _population[individual_fitnesses["individual"].to_numpy()]
     _population_fitnesses = individual_fitnesses["fitness"].to_numpy()
