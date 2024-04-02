5932abb5ca6fc193c2894d4c6c6b064b78fc92ac
Christopher Rieß <chris.mriess@gmail.com>
2024-04-02 21:34:18 +0200

    ran classic time-to-conversion evaluation

--- src/main_evaluation_ttc_classic.py
+++ src/main_evaluation_ttc_classic.py
@@ -1,9 +1,9 @@
 from data import generate_data, format_data, generate_seed
-from plot import plot_dataset
 from model import gaussian_process
 from evolution import evolve
 from kernel import classical_kernel_1
 import numpy as np
+import pandas as pd
 
 
 
@@ -18,15 +18,23 @@ def classical_gene_reader (genes):
 
 
 
+minimum_window = 5
+maximum_window = 15
+samples = 1000
+max_cycles = 15
 prediction_granularity = 2
+evaluation_data = []
 
 
 
 
-for days in range(5, 15):
-    for iteration in range(1000):
-        
-        print("====", days, "days - iteration", iteration, "====")
+for days in range(minimum_window, maximum_window):
+    
+    print("====", days, " days ====")
+    
+    iteration = 0
+    
+    while iteration < samples:
         
         seed = generate_seed(10)
         data = generate_data(days, 200, seed)
@@ -40,12 +48,23 @@ for days in range(5, 15):
             classical_gene_reader,
             7,
             prediction_granularity,
-            0.99,
-            15,
+            0.9,
+            max_cycles,
             10,
             0.5,
             0.5,
             False
         )
+        cycles += 1
         
-        print("-->", cycles)
+        print("\titeration", iteration + 1, ":", cycles, "cycles")
+        
+        if (cycles < max_cycles):
+            evaluation_data.append([seed, days, cycles])
+            iteration += 1
+            
+            
+
+
+evaluation_data = pd.DataFrame(evaluation_data, columns = ["seed", "days", "cycles"])
+evaluation_data.to_csv("data/conversions_classic.csv", index = False)
